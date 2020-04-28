22f0bdf22a6bdab76ad83fb3b49ade1c3a1e6d46
Guru K <mukhthakannan@gmail.com>
2020-04-29 04:21:48 +0530

    Update iris_eda_app.py

--- iris_eda_app.py
+++ iris_eda_app.py
@@ -5,6 +5,11 @@ def main():
 
 	st.title("Iris EDA App with streamlit")
 	st.subheader("Streamlit is Cool")
+	sentence = st.text_area("Enter Text","Type Here ..")
+	if st.button('Analyze'):
+		sid_obj = SentimentIntensityAnalyzer()
+		sentiment_dict = sid_obj.polarity_scores(sentence)
+		st.write("Overall sentiment dictionary is : ", sentiment_dict)
 
 if __name__ == '__main__':
 	main()
