57180aa01c442d2237866f27c72d6e00a5813256
Guru K <mukhthakannan@gmail.com>
2020-05-03 23:00:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,12 +3,63 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
-	
+from PIL import Image
+
 def main():
 	""" A simple Text Analyzer App """
+	st.header('Sample Analytical App on COVID19 - Guru.K')
+	st.subheader('Last Updated - Today')
+	image = Image.open('corona_image.png')
+	st.image(image, use_column_width=True)
+	st.subheader('_Statewise - Update_')
 
-	st.header('CoronaVirus(India) - Sample Analytical App- Guru.K')
-	st.subheader('_Statewise - Latest Corona Update_')
+
+	st.sidebar.header('Want to know more about COVID-19?')
+	st.sidebar.text('Yeah, Lets go!')
+	st.sidebar.subheader('Overview')
+	st.sidebar.markdown('''* Coronavirus disease (COVID-19) is an infectious disease caused by a newly discovered coronavirus.\n\n * Most people infected with the COVID-19 virus will experience mild to moderate respiratory illness and recover without requiring special treatment.\n\n* Older people, and those with underlying medical problems like cardiovascular disease, diabetes, chronic respiratory disease, and cancer are more likely to develop serious illness.''')
+	
+	st.sidebar.subheader('Symptoms')
+	st.sidebar.markdown('''Common symptoms:
+* fever
+* tiredness
+* dry cough
+\n 
+Some people may experience:
+* aches and pains.
+* nasal congestion.
+* runny nose.
+* sore throat.
+* diarrhoea.
+\n
+On average it takes 5–6 days from when someone is infected with the virus for symptoms to show, however it can take up to 14 days.
+People with mild symptoms who are otherwise healthy should self-isolate. Seek medical attention if you have a fever, a cough, and difficulty breathing. Call ahead.''')
+	st.sidebar.subheader('Prevention')
+	st.sidebar.markdown(''' STAY HOME.SAVE LIVES.
+\n
+Help stop coronavirus
+* 1STAY home
+* 2KEEP a safe distance
+* 3WASH hands often
+* 4COVER your cough
+* 5SICK? Call the helpline''')
+
+	st.sidebar.subheader('Overview')
+	st.sidebar.markdown('''Self-care
+\n
+* If you feel sick you should rest, drink plenty of fluid, and eat nutritious food. Stay in a separate room from other family members, and use a dedicated bathroom if possible. \n 
+* Clean and disinfect frequently touched surfaces.
+\n 
+* Everyone should keep a healthy lifestyle at home. Maintain a healthy diet, sleep, stay active, and make social contact with loved ones through the phone or internet. \n 
+* Children need extra love and attention from adults during difficult times. Keep to regular routines and schedules as much as possible.
+\n
+* It is normal to feel sad, stressed, or confused during a crisis. Talking to people you trust, such as friends and family, can help. If you feel overwhelmed, talk to a health worker or counsellor.
+
+Medical treatments \n
+* If you have mild symptoms and are otherwise healthy, self-isolate and contact your medical provider or a COVID-19 information line for advice.\n
+* Seek medical care if you have a fever, a cough, and difficulty breathing. Call in advance. ''')
+
+
 	df=pd.read_csv('https://api.covid19india.org/csv/latest/state_wise.csv')
 	df= df.iloc[:,:7]
 	st.dataframe(df)
@@ -99,24 +150,27 @@ def main():
 
 
 	
-	st.subheader('_Time Series - Latest Corona Update_')
+	st.subheader('Time Series Analysis')
 	df2=pd.read_csv('https://api.covid19india.org/csv/latest/case_time_series.csv')
 	st.dataframe(df2)
 
 	# plot data
 	df2=pd.read_csv('https://api.covid19india.org/csv/latest/case_time_series.csv')
 	df2[['Day','Month']] = df2.Date.apply(lambda x: pd.Series(str(x).split()))
-	df2.groupby(['Month'])['Daily Confirmed','Daily Recovered','Daily Deceased'].sum().plot(kind='line')
-	#df4
+	data=df2.groupby(['Month'])['Daily Confirmed','Daily Recovered','Daily Deceased'].sum()
+	new_order = ['January', 'February', 'March', 'April', 'May']
+	df1 = data.reindex(new_order, axis=0)
+	df1.plot(kind='line')
+	df1
 	st.pyplot()
 	
 
-	st.subheader('_Districtwise - Latest Corona Update_')
+	st.subheader('_Districtwise - Update_')
 	df3=pd.read_csv('https://api.covid19india.org/csv/latest/district_wise.csv')
 	df3=df3[['State','District','Confirmed','Active','Recovered','Deceased']]
 	#st.dataframe(df3)
 
-	#plot TreeMap
+	#Getting Unique States & User Selection:
 	unique_states=df3['State'].unique()
 	unique_states.sort()
 	option = st.selectbox('Select a State',unique_states)
@@ -149,9 +203,14 @@ def main():
 
 	st.markdown('**_Source - api.covid19india.org_**')
 
+	
+	
 	age = st.slider('Please feel free to rate this app', 1, 5, 1)
 	if st.button('Submit'):
-		st.write('Thank you for the feedback')
+		st.write('Feedback Recieved at 8667722919 & mukhthakannan@gmail.com.\n Thank you for the feedback.')
+
+	image2 = Image.open('guru_image.jpg')
+	st.image(image2, use_column_width=False)
 	
 if __name__ == '__main__':
 	main()
